10746556b87d092c010cb94a38e18f360b3a4013
Aman raj <amanraj300607@gmail.com>
2026-02-06 17:19:29 +0530

    fix error of deplyment

--- src/data/product.js
+++ src/data/product.js
@@ -1,4 +1,4 @@
-import idCard from "../assets/products/plastic Id Card Holder.jpg";
+import idCard from "../assets/products/Plastic Id Card Holder.jpg";
 import visiting from "../assets/products/brochure.jpg";
 import banner from "../assets/products/flex-banner.jpg";
 import poster from "../assets/products/poster-printing.jpg";
